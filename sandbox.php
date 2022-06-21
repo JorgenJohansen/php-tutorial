0d381a16a5f36943c200b1fae3f6ae1d9e8eea4e
jørgen buholm johansen <jorgen_buholm_johansen@hotmail.com>
2022-06-21 13:53:45 +0200

    added sessions

--- sandbox.php
+++ sandbox.php
@@ -14,6 +14,14 @@
 
     //sessions
     //server stores information
+    if(isset($_POST['submit'])){
+        session_start();
+        //storing name in session
+        $_SESSION['name'] = $_POST['name'];
+        echo $_SESSION['name'];
+
+        header('Location: index.php');
+    }
 
 
     
@@ -31,5 +39,10 @@
 </head>
 <body>
     <!-- <p><?php echo $score > 40 ?  'High score!' :  'low score :('; ?></p> -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <input type="text" name="name">
+        <input type="submit" name="submit" value="submit">
+    </form>
+
 </body>
 </html>
